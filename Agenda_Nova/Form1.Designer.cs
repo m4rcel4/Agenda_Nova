ef30bae0f44db5162512bc522ebe62c179c34b5b
MARCELA GAMA QUINTANILHA <marcela.gquintanilha@senacsp.edu.br>
2022-09-18 22:02:33 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda_Nova/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Agenda_Nova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.dgwTabela = new System.Windows.Forms.DataGridView();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.rbNome = new System.Windows.Forms.RadioButton();
+            this.rbEmail = new System.Windows.Forms.RadioButton();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwTabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(102, 477);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(135, 72);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "INSERIR";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(305, 478);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(135, 71);
+            this.btnAlterar.TabIndex = 1;
+            this.btnAlterar.Text = "ALTERAR";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(506, 478);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(132, 71);
+            this.btnDeletar.TabIndex = 2;
+            this.btnDeletar.Text = "DELETAR";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // dgwTabela
+            // 
+            this.dgwTabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwTabela.Location = new System.Drawing.Point(443, 101);
+            this.dgwTabela.Name = "dgwTabela";
+            this.dgwTabela.RowHeadersWidth = 51;
+            this.dgwTabela.RowTemplate.Height = 24;
+            this.dgwTabela.Size = new System.Drawing.Size(459, 249);
+            this.dgwTabela.TabIndex = 3;
+            this.dgwTabela.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgwTabela_MouseDoubleClick);
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(45, 86);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(325, 22);
+            this.txtPesquisar.TabIndex = 4;
+            this.txtPesquisar.TextChanged += new System.EventHandler(this.txtPesquisar_TextChanged);
+            // 
+            // rbNome
+            // 
+            this.rbNome.AutoSize = true;
+            this.rbNome.Checked = true;
+            this.rbNome.Location = new System.Drawing.Point(43, 138);
+            this.rbNome.Name = "rbNome";
+            this.rbNome.Size = new System.Drawing.Size(154, 20);
+            this.rbNome.TabIndex = 5;
+            this.rbNome.TabStop = true;
+            this.rbNome.Text = "pesquisar por NOME";
+            this.rbNome.UseVisualStyleBackColor = true;
+            // 
+            // rbEmail
+            // 
+            this.rbEmail.AutoSize = true;
+            this.rbEmail.Location = new System.Drawing.Point(212, 138);
+            this.rbEmail.Name = "rbEmail";
+            this.rbEmail.Size = new System.Drawing.Size(153, 20);
+            this.rbEmail.TabIndex = 6;
+            this.rbEmail.TabStop = true;
+            this.rbEmail.Text = "pesquisar por EMAIL";
+            this.rbEmail.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(102, 248);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(56, 22);
+            this.txtID.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(102, 307);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(213, 22);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(102, 365);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(213, 22);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "PESQUISAR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nome:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 588);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.rbEmail);
+            this.Controls.Add(this.rbNome);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.dgwTabela);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwTabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.DataGridView dgwTabela;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.RadioButton rbNome;
+        private System.Windows.Forms.RadioButton rbEmail;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Agenda_Nova/Form1.cs
@@ -0,0 +1,242 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda_Nova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mostrar();
+            btnInserir.Visible = true;
+            btnDeletar.Visible = true;
+            btnAlterar.Visible = true;
+        }
+
+
+
+        string continua = "yes";// quando iniciado o programa, os campos podem receb
+
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            verificaVazio();
+
+            if (btnInserir.Text == "INSERIR" && continua == "yes")// mudou o texto do  botão. Caso "inserir", insere novo contato.
+                                                                  // caso "novo", o MESMO botão faz outra coisa.
+            {
+
+                try
+                {
+                    using (MySqlConnection cnn = new MySqlConnection())
+                    {
+                        cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                        cnn.Open();
+                        MessageBox.Show("Inserido com sucesso!");
+                        string sql = "insert into contatos (nome, email) values ('" + txtNome.Text + "', '" + txtEmail.Text + "')";
+                        MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                        cmd.ExecuteNonQuery();
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            mostrar();
+            limpar();
+        }
+
+
+
+
+
+
+
+        private void dgwTabela_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtID.Text = dgwTabela.CurrentRow.Cells[0].Value.ToString(); // o txtID vai receber a informaação do 1º campo da tabela
+                                                                         // Cells[0] quer dizer a primeira posição
+            txtNome.Text = dgwTabela.CurrentRow.Cells[1].Value.ToString();// o txt nome vai receber informações do segundo campo da tabela
+                                                                          // Cell[1] é a segunda posição
+
+            txtEmail.Text = dgwTabela.CurrentRow.Cells[2].Value.ToString();// cells[2] é a terceira posição
+
+
+            //antes selecionar alguma coisa na tabela, o único botão a aparecer é o de inserir
+
+            btnDeletar.Visible = false;
+            btnAlterar.Visible = false;
+            // lembrando que já defini, no início do código, que todos os 3 botões aparecem
+            // mas aqui eu faço 2 sumirem, somente.
+
+            btnInserir.Text = "NOVO"; // o botão inserir não pode funcionar quando já selecionei algo na tabela
+
+            txtPesquisar.Text = ""; // faz sumir algo digitado anteriormente, limpa o txt
+
+
+        }
+
+
+        void limpar()
+        {
+
+            txtID.Text = "";
+            txtNome.Text = "";
+            txtEmail.Text = "";
+            btnInserir.Text = "INSERIR";
+            btnDeletar.Visible = false;
+            btnAlterar.Visible = false;
+        }
+
+
+
+
+        void mostrar() // void mostrar é mostrar todas as informações do banco
+        {
+            try
+            {
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+                    cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                    cnn.Open();
+                    string sql = "Select * from contatos";
+                    DataTable table = new DataTable();
+                    MySqlDataAdapter adpter = new MySqlDataAdapter(sql, cnn);
+                    adpter.Fill(table);
+                    dgwTabela.DataSource = table;
+
+                    dgwTabela.AutoGenerateColumns = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+
+
+        void verificaVazio() // void criado para impedir que campos vazios sejam inseridos no banco
+        {
+            if (txtNome.Text == "" || txtEmail.Text == "") // se o nome OU o email estiverem vazios, ele não insere
+            {
+                continua = "no"; // essa é uma variável criada no ínicio do código, usada no botão de inserir.
+                MessageBox.Show("Preencha todos os campos");
+            }
+            else
+            {
+                continua = "yes";
+            }
+        }
+
+
+
+
+
+
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Deseja excluir contato?", "Confirmação", MessageBoxButtons.YesNo))
+            // criei uma messageBox, caso confirme que deseja excluir, o comando abaixo é realizado 
+
+            {
+                try
+                {
+                    using (MySqlConnection cnn = new MySqlConnection())
+                    {
+                        cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                        cnn.Open();
+                        string sql = "Delete from contatos where idContatos = '" + txtID.Text + "'";
+                        MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show(" Deletado com sucesso! ");
+
+                    }
+                    limpar();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            mostrar();
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+                    cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                    cnn.Open();
+                    string sql = "Update contatos set nome='" + txtNome.Text + "', email='" + txtEmail.Text + "' where idContatos='" + txtID.Text + "'";
+                    MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Atualizado com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            mostrar();
+        }
+
+        private void txtPesquisar_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+                    cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                    cnn.Open();
+                    string sql;
+
+                    if (rbEmail.Checked)
+                    {
+                        sql = "Select * from contatos where email like'" + txtPesquisar.Text + "%'";
+                    }
+                    else // não defini o rbNome no if porque ele já vai estar selecionado automaticamente kkkkk
+                    {
+                        sql = "Select * from contatos where nome Like'" + txtPesquisar.Text + "%'";
+                    }
+
+                    MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                    cmd.ExecuteNonQuery();
+                    DataTable table = new DataTable();
+                    MySqlDataAdapter adpter = new MySqlDataAdapter(sql, cnn);
+                    adpter.Fill(table);
+                    dgwTabela.DataSource = table;
+
+                    dgwTabela.AutoGenerateColumns = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+    }
+}
